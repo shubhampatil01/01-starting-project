ce01fc736f8d480b1e05b52e053e078ffe7ca9b2
shubhampatil.ani@gmail.com <shubhampatil.ani@gmail.com>
2023-02-11 16:30:46 +0530

    adding extra (college)input

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const storedUserLoginInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoginInformation === 1) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
-  const loginHandler = (email, password) => {
+  const loginHandler = (email, password, college) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
